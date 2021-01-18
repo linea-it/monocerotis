e8b7cf4d94ad04793eb115d7224f4a9f8f6d7597
Fernando Silva <fernando_nicolau@outlook.com>
2021-01-18 18:29:43 -0300

    feat : Beginning of the project.

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import { BrowserRouter, Switch } from 'react-router-dom';
-import Route from './Route';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// import Header from '../components/Header';
+// import Footer from '../components/Footer';
 import Home from '../pages/Home';
-import Registration from '../pages/Registration';
-import Program from '../pages/Program';
-import AdditionalInformation from '../pages/AdditionalInformation';
-import Notfound from '../pages/NotFound';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+// import AboutUs from '../pages/AboutUs';
+// import Help from '../pages/Help';
+// import Tutorials from '../pages/Tutorials';
+// import Contact from '../pages/Contact';
+// import OracleEasyAccess from '../pages/OracleEasyAccess';
+// import Notfound from '../pages/NotFound';
 
-export default function Routes() {
+function Router() {
   return (
     <BrowserRouter>
-      <Header />
+      {/* <Header /> */}
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/registration" component={Registration} />
-        <Route exact path="/program" component={Program} />
-        <Route exact path="/additional-information" component={AdditionalInformation} />
-        <Route component={Notfound} />
+        {/* <Route path="/about-us" component={AboutUs} />
+        <Route path="/help" component={Help} />
+        <Route path="/tutorials" component={Tutorials} />
+        <Route path="/contact-us" component={Contact} />
+        <Route path="/oracle-easy-access" component={OracleEasyAccess} />
+        <Route component={Notfound} /> */}
       </Switch>
-      <Footer />
+      {/* <Footer /> */}
     </BrowserRouter>
   );
 }
+
+export default Router;
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,19 +1,106 @@
+/* eslint-disable no-console */
+/* eslint-disable import/prefer-default-export */
 import axios from 'axios';
 
-// Set the default baseURL for the API
-axios.defaults.baseURL = process.env.REACT_APP_API;
+const host = process.env.REACT_APP_API || `${window.location.protocol}//${window.location.hostname}${window.location.port ? ':' : ''
+  }${window.location.port}`;
 
-export const postRegistration = ({ name, email, institute, newsletter }) => {
-  const params = { name, email, institute, newsletter_permission: newsletter };
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = `${host}/dri/api/`;
 
-  return axios.post('/subscription/', params)
-    .then(res => res)
-    .catch(err => err)
-}
-export const getRegistrations = () => {
+// Interceptar a Resposta.
+// Add a response interceptor
+axios.interceptors.response.use(
+  (response) => (
+    // Do something with response data
+    response
+  ),
+  (error) => {
+    // Do something with response error
+    if (error.response) {
+      switch (error.response.status) {
+        case 401:
+          // Na landinpage o Login é opicional. não é necessário redirecionar para o login.
+          break;
+        case 403:
+          // Na landinpage o Login é opicional. não é necessário redirecionar para o login.
+          break;
+        case 404:
+          to404();
+          break;
+        default:
+          console.log(error.request);
+      }
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.log(error.request);
+      // to404();
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log(error.message);
+      // to404();
+    }
 
-  return axios.get('/subscription/')
-    .then(res => res)
-    .catch(err => err)
-}
 
+    return Promise.reject(error);
+  },
+);
+
+// eslint-disable-next-line consistent-return
+export const getLoggedUser = () => axios.get('logged/get_logged/', { params: { format: 'json' } }).then((result) => {
+
+  var data = result.data;
+
+  // Informa o Id o usuario para o GA, para que possa reconher usuarios unicos.
+  window.gtag('config', 'GA_MEASUREMENT_ID', {
+    'user_id': data.id
+  });
+
+  return data
+}).catch((error) => {
+  if (error) {
+    return { username: undefined };
+  }
+  if (error.response.status === 403) {
+    return { username: undefined };
+  }
+});
+
+export const tutorials = () => axios.get('tutorial/')
+  .then((res) => res.data)
+  .catch((err) => {
+    console.error(err);
+    return err;
+  });
+
+export const getApplication = () => axios.get('application/?format=json&ordering=app_order')
+  .then((res) => res.data.filter((el) => !el.app_disabled))
+  .catch((err) => {
+    console.error(err);
+    return err;
+  });
+
+export const sendEmail = (formData) => axios.post('contact/', formData)
+  .then((res) => res)
+  .catch((err) => err);
+
+export const signPath = () => axios.get('get_ncsa_signup/')
+  .then((res) => res.data)
+  .catch((err) => {
+    console.error(err);
+    return err;
+  });
+
+const to404 = () => {
+  window.open(`${host}/404`);
+};
+
+export const urlLogin = `${host}/dri/api/api-auth/login/?next=/`;
+
+export const urlSign = (path) => {
+  return path ? `${host}/dri/api/${path}` : `${host}/oracle-easy-access`;
+};
+
+export const urlLogout = `${host}/dri/api/api-auth/logout/?next=/`;
